95265fcdda760bd6281e99b7d3ed7149e37f689c
AndroidStudio-App <androidstudio441@gmail.com>
2026-05-13 16:32:11 +0800

    fix: external logging

--- app/src/main/java/com/neonide/studio/app/NeonIDEApplication.java
+++ app/src/main/java/com/neonide/studio/app/NeonIDEApplication.java
@@ -3,6 +3,9 @@ package com.neonide.studio.app;
 import android.content.Context;
 import com.termux.app.TermuxApplication;
 
+import com.neonide.studio.logger.IDEFileLogger;
+import com.termux.shared.logger.Logger;
+
 public class NeonIDEApplication extends TermuxApplication {
 
     @Override
@@ -14,5 +17,10 @@ public class NeonIDEApplication extends TermuxApplication {
         // Init sora-editor file providers
         io.github.rosemoe.sora.langs.textmate.registry.FileProviderRegistry.getInstance().addFileProvider(new io.github.rosemoe.sora.langs.textmate.registry.provider.AssetsFileResolver(context.getAssets()));
         io.github.rosemoe.sora.langs.monarch.registry.FileProviderRegistry.INSTANCE.addProvider(new io.github.rosemoe.sora.langs.monarch.registry.provider.AssetsFileResolver(context.getAssets()));
+
+        // Register IDEFileLogger to capture all logs sent via Logger class
+        Logger.setExternalLogger((priority, tag, message) -> {
+            IDEFileLogger.log(context, tag + ": " + message);
+        });
     }
 }
